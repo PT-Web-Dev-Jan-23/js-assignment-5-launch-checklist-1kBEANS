980e915d58aaf2d820614ccd3928abee47673048
1kBEANS <robinsonjoe913@gmail.com>
2023-03-18 12:19:44 -0500

    rags and tatters, rags and tatters

--- script.js
+++ script.js
@@ -16,9 +16,9 @@ window.addEventListener("load", function() {
     }
    });
 
-   let listedPlanets= myFetch().value;
+   let listedPlanets;
    // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
+   let listedPlanetsResponse = myFetch();
    listedPlanetsResponse.then(function (result) {
        listedPlanets = result;
        console.log(listedPlanets);
@@ -26,7 +26,9 @@ window.addEventListener("load", function() {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
        let randomPlanet = pickPlanet(listedPlanets);
-       addDestinationInfo(randomPlanet);
+       let destination = addDestinationInfo(randomPlanet);
+
+       console.log(destination);
    })
    
 });
